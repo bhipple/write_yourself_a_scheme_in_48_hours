75a88662bcafcc2f05d9e037e2966fd159d48221
Benjamin Hipple <brh2113@columbia.edu>
2015-05-18 15:53:26 -0400

    Cleanup

--- first_steps.hs
+++ first_steps.hs
@@ -7,15 +7,7 @@ main = do
     -- Say hello to each arg
     printElements args
 
-    -- Print the sum of each char in the string as a number
-    printSums args
-
 printElements :: [String] -> IO()
 printElements [] = return ()
 printElements (x:xs) = do putStrLn("Hello, " ++ x ++ "\n")
                           printElements xs
-
-printSums :: [String] -> IO()
-printSums [] = return ()
-printSums (x:xs) = do read x
-                      printSums(xs)
