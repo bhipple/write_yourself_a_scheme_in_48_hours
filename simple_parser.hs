5b8f2cb4a5015754d4b55e22c8f833349e3117a7
Benjamin Hipple <brh2113@columbia.edu>
2015-06-01 23:56:22 -0400

    Functions and closures!

--- simple_parser.hs
+++ simple_parser.hs
@@ -83,6 +83,12 @@ showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal (PrimitiveFunc _) = "<primitive>"
+showVal (Func {params = args, vararg = varargs, body = body, closure = env}) =
+        "(lambda (" ++ unwords (map show args) ++
+         (case varargs of
+            Nothing -> ""
+            Just arg -> " . " ++ arg) ++ ") ...)"
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
@@ -157,21 +163,35 @@ eval env (List [Atom "set!", Atom var, form]) =
      eval env form >>= setVar env var
 eval env (List [Atom "define", Atom var, form]) =
      eval env form >>= defineVar env var
+eval env (List (Atom "define" : List (Atom var : params) : body)) =
+        makeNormalFunc env params body >>= defineVar env var
+eval env (List (Atom "define" : DottedList (Atom var : params) varargs : body)) =
+        makeVarArgs varargs env params body >>= defineVar env var
+eval env (List (Atom "lambda" : List params : body)) =
+        makeNormalFunc env params body
+eval env (List (Atom "lambda" : varargs@(Atom _) : body)) =
+        makeVarArgs varargs env [] body
 -- The : is the cons operator. This gives ups (x:xs) behavior, where x is
 -- an Atom. We recursively evaluate all argument lists for further
 -- functions, to handle things like (+ 2 (* 3 4) 5) = 19
-eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
+eval env (List (function : args)) = do
+        func <- eval env function
+        argVals <- mapM (eval env) args
+        apply func argVals
 eval env badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
--- Given a key func, we lookup its value in primites.
--- If this isn't found, the function returns false. Otherwise,
--- it applies the primitive operator to args using the function application
--- operator $
-apply :: String -> [LispVal] -> ThrowsError LispVal
---apply func args = maybe (Bool False) ($ args) $ lookup func primitives
-apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
-                        ($ args)
-                        (lookup func primitives)
+apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
+apply (PrimitiveFunc func) args = liftThrows $ func args
+apply (Func params varargs body closure) args =
+        if num params /= num args && varargs == Nothing
+            then throwError $ NumArgs (num params) args
+            else (liftIO $ bindVars closure $ zip params args) >>= bindVarArgs varargs >>= evalBody
+        where remainingArgs = drop (length params) args
+              num = toInteger . length
+              evalBody env = liftM last $ mapM (eval env) body
+              bindVarArgs arg env = case arg of
+                    Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
+                    Nothing -> return env
 
 -- List of pairs, the keys are Strings and the values are functions mapping
 -- [LispVal] -> LispVal. This lookup will return a numericBinop which has
@@ -351,6 +371,12 @@ bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
           addBinding (var, value) = do ref <- newIORef value
                                        return (var, ref)
 
+-- Using first class functions, makeNormalFunc and makeVarArgs are
+-- specializations of makeFunc
+makeFunc varargs env params body = return $ Func (map showVal params) varargs body env
+makeNormalFunc = makeFunc Nothing
+makeVarArgs = makeFunc . Just . showVal
+
 -----------------------------------------------------
 -- REPL
 flushStr :: String -> IO ()
@@ -374,11 +400,16 @@ until_ pred prompt action = do
         then return ()
         else action result >> until_ pred prompt action
 
+-- On startup, map all the default primitive functions into the environment
+primitiveBindings :: IO Env
+primitiveBindings = nullEnv >>= (flip bindVars $ map makePrimitiveFunc primitives)
+    where makePrimitiveFunc (var, func) = (var, PrimitiveFunc func)
+
 runOne :: String -> IO ()
-runOne expr = nullEnv >>= flip evalAndPrint expr
+runOne expr = primitiveBindings >>= flip evalAndPrint expr
 
 runRepl :: IO ()
-runRepl = nullEnv >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
+runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
 
 main :: IO ()
 main = do args <- getArgs
